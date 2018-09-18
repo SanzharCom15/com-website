d82187d5d3241983e588f0de4f5b5494787f4f84
Sanzhar <sanzhar.zhenishbekuulu@iaau.edu.kg>
2018-09-18 23:54:18 +0600

    body background changed

--- js/scroll.js
+++ js/scroll.js
@@ -4,6 +4,7 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
     document.getElementById("navbar").style.background= "#008B8B";
+
   } else {
     document.getElementById("navbar").style.background= "transparent";
   }
